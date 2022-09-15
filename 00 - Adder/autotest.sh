3b65e900cd6bf2f3bc4c05a5afc254f183b35c98
abaur <abaur@student.42.fr>
2022-09-15 20:04:42 +0200

    00 autotest

---
+++ 00 - Adder/autotest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make all \
+&& ./adder.out 0 1 2 3 4 5 \
+&& ./adder.out -1 +1 -1 -5 +21 -125 +341\
+&& ./adder.out 1 3 7 15 31 63 127 255 \
+&& ./adder.out 1 5 21 125 341 \
+&& ./adder.out 1 9 73 585 4681 \
+&& ./adder.out 0 3 27 219 3510 \
+;
