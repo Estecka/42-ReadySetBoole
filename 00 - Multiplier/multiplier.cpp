9a1c9e109b15983c68822ab1e2e46d7da07ee4a1
abaur <abaur@student.42.fr>
2022-09-16 18:45:16 +0200

    01 Working multiplier with no "if"

--- 00 - Multiplier/multiplier.cpp
+++ 00 - Multiplier/multiplier.cpp
@@ -6,18 +6,22 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 17:07:02 by abaur             #+#    #+#             */
-/*   Updated: 2022/09/16 17:41:52 by abaur            ###   ########.fr       */
+/*   Updated: 2022/09/16 18:42:04 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../00 - Adder/adder.hpp"
 
+/**
+ * Converts 1 into -1, and 0 into 0;
+ */
+#define TRUTH(n)	( (signed int)((n)<<31) >> 31 )
+
 extern unsigned int	multiplier(unsigned int a, unsigned int b){
 	unsigned int r = 0;
 
 	for (unsigned int i=1; i; i<<=1) {
-		if (b & 1)
-			r = adder(r, a);
+		r = adder(r, a & TRUTH(b & 1));
 
 		b >>= 1;
 		a <<= 1;
