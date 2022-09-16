9e2e4361630e964029581a54714a454e843c84ee
abaur <abaur@student.42.fr>
2022-09-16 17:43:23 +0200

    01 Working multiplier with a "if"

--- 00 - Adder/adder.cpp
+++ 00 - Adder/adder.cpp
@@ -6,14 +6,14 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 17:57:59 by abaur             #+#    #+#             */
-/*   Updated: 2022/09/15 19:53:47 by abaur            ###   ########.fr       */
+/*   Updated: 2022/09/16 17:19:12 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 extern unsigned int	adder(unsigned int a, unsigned int b){
 	unsigned int carry;
 
-	for (unsigned int i=1; i|0; i<<=1) {
+	for (unsigned int i=1; i; i<<=1) {
 		carry = (a & b) << 1;
 		a ^= b;
 		b = carry;
--- 00 - Multiplier/multiplier.cpp
+++ 00 - Multiplier/multiplier.cpp
@@ -6,10 +6,22 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 17:07:02 by abaur             #+#    #+#             */
-/*   Updated: 2022/09/16 17:07:04 by abaur            ###   ########.fr       */
+/*   Updated: 2022/09/16 17:41:52 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../00 - Adder/adder.hpp"
+
 extern unsigned int	multiplier(unsigned int a, unsigned int b){
-	return a;
+	unsigned int r = 0;
+
+	for (unsigned int i=1; i; i<<=1) {
+		if (b & 1)
+			r = adder(r, a);
+
+		b >>= 1;
+		a <<= 1;
+	}
+	
+	return r;
 }
