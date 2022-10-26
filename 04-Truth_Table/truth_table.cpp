a9de34e541c0ac5f8d4cec1f0fe30752ee666fb7
abaur <abaur@student.42.fr>
2022-10-26 21:29:41 +0200

    04 compare_truth_table

--- 04-Truth_Table/truth_table.cpp
+++ 04-Truth_Table/truth_table.cpp
@@ -6,51 +6,93 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 19:19:56 by abaur             #+#    #+#             */
-/*   Updated: 2022/10/26 19:15:04 by abaur            ###   ########.fr       */
+/*   Updated: 2022/10/26 21:28:41 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "truth_table.hpp"
 
 #include "../03-Boolean_Evaluation/eval_formula.hpp"
+#include "../logutil/logcolors.hpp"
 #include <iostream>
+#include <sstream>
 
-static void	Header(std::ostream& cout, const std::string& expr, short& varcount, char shiftmap[26]){
+/**
+ * @param outVarcount	Outputs the number of different variable names in this 
+ * 	expression
+ * @param outShiftmap	Outputs  a  shiftmap,  which  can  be  used  to  compute
+ * 	a different varmap from each row index in this table.
+ */
+static void	Header(std::ostream& cout, const std::string& expr, short& outVarcount, char outShiftmap[26]){
 	bool	exists[26] = { false };
 
 	for (const char* str=expr.c_str(); *str; str++)
 	if ('A' <= *str && *str <= 'Z' && !exists[*str - 'A']){
 		exists[*str - 'A'] = true;
-		shiftmap[varcount] = *str;
-		varcount++;
+		outShiftmap[outVarcount] = *str;
+		outVarcount++;
 		cout << "| " << *str << " ";
 	}
-
-	cout << "| = |" << std::endl;
 }
 
-static void	Row(std::ostream& cout, short i, short varcount, const char* shiftmap, const std::string& expr){
-	bool varmap[26] = { 0 }; // Translates variable name into its value;
-
+/**
+ * @param outVarmap	outputs a varmap, which converts a variable's name into its
+ * 	value for this row.
+ */
+static void	Row(std::ostream& cout, short i, short varcount, const char* shiftmap, bool (&outVarmap)[26]){
 	for (int j=0; j<varcount; j++){
 		bool value = 1 & (i>>j);
-		varmap[shiftmap[j] - 'A'] = value;
+		outVarmap[shiftmap[j] - 'A'] = value;
 		cout << "| " << (int)value << " ";
 	}
-	cout << "| " << eval_formula(expr, varmap) << " |" << std::endl;
+	cout << "|";
 }
 
 extern void	print_truth_table(const std::string& expr) {
 	short	varcount = 0 ;
 	char	shiftmap[26] = { 0 }; // Translates variable column index into its name.
+	bool	varmap[26] = { 0 };
 
 
 	Header(std::cout, expr, varcount, shiftmap);
+	std::cout << "| = |" << std::endl;
 
 	for (int i=0; i<varcount; i++)
 		std::cout << "|---";
 	std::cout << "|---|" << std::endl;
 
-	for (int i=0; i<(1<<varcount); i++)
-		Row(std::cout, i, varcount, shiftmap, expr);
+	for (int i=0; i<(1<<varcount); i++){
+		Row(std::cout, i, varcount, shiftmap, varmap);
+		std::cout <<  " " << (int)eval_formula(expr, varmap) << " |" << std::endl;
+	}
+}
+
+extern bool	compare_truth_tables(const std::string& a, const std::string& b, std::string& outTable){
+	short	varcount = 0 ;
+	char	shiftmap[26] = { 0 }; // Translates variable column index into its name.
+	bool	varmap[26] = { 0 };
+	bool	diff = false;
+	std::stringstream cout;
+
+
+	Header(cout, a+b, varcount, shiftmap);
+	cout << "| src |" LOG_CYAN " nnf |" LOG_CLEAR << std::endl;
+
+	for (int i=0; i<varcount; i++)
+		cout << "|---";
+	cout << "|-----|" LOG_CYAN "-----|" LOG_CLEAR << std::endl;
+
+	for (int i=0; i<(1<<varcount); i++){
+		Row(cout, i, varcount, shiftmap, varmap);
+		bool ra = eval_formula(a, varmap);
+		bool rb = eval_formula(b, varmap);
+		diff |= (ra == rb);
+		cout << "  " << (int)ra << "  |"
+		          << ((ra == rb) ? LOG_CYAN : LOG_BOLD_RED)
+		          << "  " << (int)rb << LOG_CYAN "  |"
+		          << LOG_CLEAR << std::endl;
+	}
+
+	outTable = cout.str();
+	return diff;
 }
--- 04-Truth_Table/truth_table.hpp
+++ 04-Truth_Table/truth_table.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:22:35 by abaur             #+#    #+#             */
-/*   Updated: 2022/10/26 19:04:25 by abaur            ###   ########.fr       */
+/*   Updated: 2022/10/26 21:27:03 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 
 void	print_truth_table(const std::string& str);
 
+/**
+ * @return true if the expression results differ in any case.
+ */
 bool	compare_truth_tables(const std::string& a, const std::string& b, std::string& outTable);
 
 #endif
