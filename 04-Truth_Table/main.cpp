c9e3e21fb263cbfd3f946d61cf6b9b42962acbba
abaur <abaur@student.42.fr>
2022-10-29 19:48:36 +0200

    04 Added --compare option to tester

--- 04-Truth_Table/main.cpp
+++ 04-Truth_Table/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:21:03 by abaur             #+#    #+#             */
-/*   Updated: 2022/10/26 18:50:38 by abaur            ###   ########.fr       */
+/*   Updated: 2022/10/29 19:41:53 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 
-extern int	main(int argc, char** argv){
-	for (int i=1; i<argc; i++) {
-		IPolishItem* tree;
+static void	List(int argc, const char*const* argv){
+	for (int i=0; i<argc; i++) {
+		IPolishItem* tree = NULL;
 		std::string remainder;
 
 		std::cout << argv[i] << std::endl;
@@ -34,5 +35,46 @@ extern int	main(int argc, char** argv){
 		catch(const std::exception& e) {
 			std::cout << LOG_YELLOW << e.what() << LOG_CLEAR"\n" << std::endl;
 		}
+		if (tree) delete tree;
 	}
+}
+
+
+static void	Compare(const char* base, int argc, const char*const* argv){
+	IPolishItem* tree;
+	std::string remainder;
+	std::cout << base << std::endl;
+
+	BuildTree(base, tree, remainder);
+	if (remainder.length())
+		ft::clog << ft::log::warning << "Base expression has trailing characters" << std::endl;
+	std::cout << tree->Draw();
+	delete tree;
+
+	for (int i=0; i<argc; i++) {
+		std::cout << LOG_BOLD_CYAN "\n" << argv[i] << LOG_CLEAR << std::endl;
+
+		tree = NULL;
+		BuildTree(argv[i], tree, remainder);
+		if (remainder.length())
+			ft::clog << ft::log::warning << "Expression has trailing characters" << std::endl;
+		std::cerr << LOG_CYAN << tree->Draw() << LOG_CLEAR;
+
+		compare_truth_tables(base, argv[i], remainder);
+		std::cout << remainder;
+		delete tree;
+	}
+}
+
+extern int	main(int argc, char** argv){
+	if (argc < 2 || !std::strcmp(argv[1], "--compare")){
+		if (argc < 4){
+			std::cerr << "Not enough arguments" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		else
+			Compare(argv[2], argc-3, argv+3);
+	}
+	else
+		List(argc-1, argv+1);
 }
--- 04-Truth_Table/negational_forms.sh
+++ 04-Truth_Table/negational_forms.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 make all \
-&& echo -e '----\n' && ./truth_table.out 'A!!' 'A' \
-&& echo -e '----\n' && ./truth_table.out 'AB&!' 'A!B!|' \
-&& echo -e '----\n' && ./truth_table.out 'AB|!' 'A!B!&' \
-&& echo -e '----\n' && ./truth_table.out 'AB^!' 'AB&A!B!&|' \
-&& echo -e '----\n' && ./truth_table.out 'AB=!' 'A!B!|AB|&' \
-&& echo -e '----\n' && ./truth_table.out 'AB>!' 'AB!&' \
-&& echo -e '----\n' && ./truth_table.out 'AB>' 'A!B|' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'A!!' 'A' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'AB&!' 'A!B!|' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'AB|!' 'A!B!&' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'AB^!' 'AB&A!B!&|' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'AB=!' 'A!B!|AB|&' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'AB>!' 'AB!&' \
+&& echo -e '\n----\n' && ./truth_table.out --compare 'AB>' 'A!B|' \
 ;
