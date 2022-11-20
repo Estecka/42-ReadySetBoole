7b52fac3ea05e9e6585e218985a3868b631cd4a8
abaur <abaur@student.42.fr>
2022-11-20 19:20:11 +0100

    08 Fix set_to_bit not working past the second chunk

--- 09-Set_Evaluation/autotest.sh
+++ 09-Set_Evaluation/autotest.sh
@@ -2,17 +2,18 @@
 
 make all \
 && echo -e "\n\t# BASE EXPRESSIONS" \
-&& ./eval_set.out "0"   "0,1" "1,2" "" \
-&& ./eval_set.out "1"   "0,1" "1,2" "" \
-&& ./eval_set.out "A"   "0,1" "1,2" \
-&& ./eval_set.out "B!"  "1" "2,3" "4,5"\
-&& ./eval_set.out "AB&" "7,6,5" "6,5,4" \
-&& ./eval_set.out "CD|" '0,1,2' '' "5,3,4" "5,6,7" \
-&& ./eval_set.out "BC^" '0' "1,2" "2,3" \
-&& ./eval_set.out "CD=" '0' '' "3,4,5" "5,6" \
-&& ./eval_set.out "AC>" "6,7,8" '1,9' "8,9,10" \
+&& ./eval_set.out "0" '0,1' '1,2' '' \
+&& ./eval_set.out "1" '0,1' '1,2' '' \
+&& ./eval_set.out "A" '0,1' '1,2' \
+&& ./eval_set.out "A" '0,1,2,3,4,5,6,7,8,9' \
+&& ./eval_set.out "B!"  '1' '2,3' '3,4,5'\
+&& ./eval_set.out "AB&" '7,6,5' '6,5,4' \
+&& ./eval_set.out "CD|" '0,1,2,9' '' '5,3,4' '5,6,7' \
+&& ./eval_set.out "BC^" '0' '1,2' '2,3' \
+&& ./eval_set.out "CD=" '0' '' '3,4,5' '5,6' \
+&& ./eval_set.out "AC>" '6,7,8' '1,9' '8,9,10' \
 && echo -e "\n\t# COMPLEX EXPRESSIONS" \
-&& ./eval_set.out "AA^BB!=|!" "0,1" "2,3" \
-&& ./eval_set.out "AB&CD|!^" "0,1,9" "1,2" "3,0" "0,5" \
+&& ./eval_set.out "AA^BB!=|!" '0,1' '2,3' \
+&& ./eval_set.out "AB&CD|!^" '0,1,9' '1,2' '3,0' '0,5' \
 && ./eval_set.out "ABCDE||||" '0,5' '1,6' '2,7' '3,8' '4,9' \
 ;
--- 09-Set_Evaluation/bitstring.cpp
+++ 09-Set_Evaluation/bitstring.cpp
@@ -6,24 +6,31 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 15:45:47 by abaur             #+#    #+#             */
-/*   Updated: 2022/11/20 17:46:17 by abaur            ###   ########.fr       */
+/*   Updated: 2022/11/20 19:18:42 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitstring.hpp"
+
+// #include <iostream>
+
+#define sizeof_bitchunk	(8*sizeof(bitstring::value_type))
 
 bitstring	vec_to_bit(const std::set<int>& universe, const std::vector<int>& vec){
 	return set_to_bit(universe, std::set<int>(vec.begin(), vec.end()));
 }
 bitstring	set_to_bit(const std::set<int>& universe, const std::set<int>& set){
 	bitstring result;
-
-	std::set<int>::const_iterator elt = universe.begin();
-	for (int n=0; (n*8*sizeof(bitstring::value_type))<universe.size(); n++){
+	for (size_t i=0; i<universe.size(); i+=sizeof_bitchunk)
 		result.push_back(0);
-		for (int i=0; (n*sizeof(int)+i)<universe.size(); i++, elt++) {
-			if (set.find(*elt) != set.end())
-				result[n] |= 1 << i;
+
+	size_t n=0, i=0;
+	for (std::set<int>::const_iterator elt=universe.begin(); elt!=universe.end(); elt++, i++){
+		if (i >= sizeof_bitchunk)
+			n++, i-= sizeof_bitchunk;
+		if (set.find(*elt) != set.end()) {
+			result[n] |= 1 << i;
+			// std::cerr << n << " " << i << " = " << *elt << std::endl;
 		}
 	}
 
@@ -33,11 +40,14 @@ bitstring	set_to_bit(const std::set<int>& universe, const std::set<int>& set){
 std::vector<int>	bit_to_vec(const std::set<int>& universe, const bitstring& bits){
 	std::vector<int> result;
 
-	std::set<int>::const_iterator elt = universe.begin();
-	for (int n=0; (n*8*sizeof(bitstring::value_type))  <universe.size(); n++)
-	for (int i=0; (n*8*sizeof(bitstring::value_type)+i)<universe.size(); i++, elt++) {
-		if ((bits[n] >> i) & 1)
+	size_t n=0, i=0;
+	for (std::set<int>::const_iterator elt=universe.begin(); elt!=universe.end(); elt++, i++){
+		if (i >= sizeof_bitchunk)
+			n++, i-= sizeof_bitchunk;
+		if ((bits[n] >> i) & 1){
 			result.push_back(*elt);
+			// std::cerr << n << " " << i << " = " << *elt << std::endl;
+		}
 	}
 
 	return result;
